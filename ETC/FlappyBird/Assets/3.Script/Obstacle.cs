b90b782e9c6d187c253d562dc68570a91a017aa4
Hansiban <dbrud3073@kakao.com>
2023-10-21 15:00:57 +0900

    [Update] flippy bird update
    player 움직임 구현 완료
    object poolling 구현 중

--- ETC/FlappyBird/Assets/3.Script/Obstacle.cs
+++ ETC/FlappyBird/Assets/3.Script/Obstacle.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    //z축으로 전진
-    //z축이 일정 이상 넘어오면 다시 큐에 넣기
+    [SerializeField] float speed;
+    [SerializeField] float Z_posEnd;
+    [SerializeField] ObstacleSpawn obstacleSpawn;
+    private Rigidbody rigid;
+
+    private void Awake()
+    {
+        TryGetComponent(out rigid);
+        obstacleSpawn = GameObject.FindWithTag("Player").GetComponent<ObstacleSpawn>();
+    }
+    private void Update()
+    {
+        if (gameObject.transform.position.z <= Z_posEnd)
+        {
+            obstacleSpawn.InObstacle(gameObject);
+        }
+        //z축으로 전진
+        rigid.AddForce(Vector3.back * speed);
+    }
 }
--- ETC/FlappyBird/Assets/3.Script/ObstacleSpawn.cs
+++ ETC/FlappyBird/Assets/3.Script/ObstacleSpawn.cs
@@ -2,12 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
+public class ObstacleSpawn : MonoBehaviour
 {
-    [SerializeField] GameObject prefab;
-    
-    // 큐에 장애물 생성(생성 시 y랜덤으로 받기)
-    // 꺼내기
-    // 다시 넣기
+    [Header("오브젝트 풀링")]
+    [SerializeField] GameObject obstacle;
+    [SerializeField] float count;
+    [SerializeField] float spawntime;
+    private Queue<GameObject> obstacleQueue;
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] float maxY;
+    [SerializeField] float minY;
 
+    private void Awake()
+    {
+        startPosition = transform.position;
+        maxY = 5;
+        minY = -5;
+
+        // 큐에 count만큼 장애물 생성
+        obstacleQueue = new Queue<GameObject>();
+        for (int i = 0; i < count; i++)
+        {
+            GameObject obj = Instantiate(obstacle, transform.position, transform.rotation);
+            obj.SetActive(false);
+            obstacleQueue.Enqueue(obj);
+        }
+
+        //코루틴 시작
+        SpawnEnemy_Co();
+    }
+
+    private IEnumerator SpawnEnemy_Co()
+    {
+        WaitForSeconds wfs = new WaitForSeconds(spawntime);
+        while (true)
+        {
+            float positionY = Random.Range(minY, maxY);
+            OutObstacle(positionY);
+            yield return wfs;
+        }
+    }
+
+    public void InObstacle(GameObject obstacle) //넣기
+    {
+        //포지션, 활성화 설정하기
+        obstacle.SetActive(false);
+        obstacle.transform.position = transform.position;
+
+        //넣기
+        obstacleQueue.Enqueue(obstacle);
+    }
+
+    public void OutObstacle(float positionY) //빼기
+    {
+        //큐에 장애물 없으면 return
+        if (obstacleQueue.Count <= 0)
+        {
+            return;
+        }
+
+        //꺼내기
+        GameObject obj = obstacleQueue.Dequeue();
+
+        //포지션, 활성화 설정하기
+        obstacle.SetActive(true);
+        obj.transform.position = startPosition + new Vector3(0, positionY, 0);
+    }
 }
